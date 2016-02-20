8d73e04d719651436197488e0c52a8653e360ced
hyungchae <kimhyungchae@gmail.com>
2016-02-20 10:14:50 +0900

    #37  MenuVo -> Menu로 변경

--- wu-zu-home/src/test/java/com/wuzu/jpa/test/MenuCrudTest.java
+++ wu-zu-home/src/test/java/com/wuzu/jpa/test/MenuCrudTest.java
@@ -1,9 +1,9 @@
 package com.wuzu.jpa.test;
 
 import com.wuzu.jpa.annotation.TransactionalDevTest;
-import com.wuzu.jpa.domain.MenuVo;
-import com.wuzu.jpa.domain.QMenuVo;
-import com.wuzu.jpa.service.MenuCrudService;
+import com.wuzu.home.domain.Menu;
+import com.wuzu.home.domain.QMenu;
+import com.wuzu.web.ui.menu.service.MenuCrudService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
@@ -36,11 +36,11 @@ public class MenuCrudTest {
 
     @Test
     public void createTest() {
-        MenuVo menuVo = new MenuVo();
+        Menu menuVo = new Menu();
         menuVo.setName("folder 3");
         menuVo.setOpened(true);
 
-        MenuVo rtnMenuVo = this.menuService.create(menuVo);
+        Menu rtnMenuVo = this.menuService.create(menuVo);
         assertThat(rtnMenuVo.getName()).isEqualTo("folder 3");
         assertThat(rtnMenuVo.isOpened()).isEqualTo(true);
 
@@ -49,7 +49,7 @@ public class MenuCrudTest {
 
     @Test
     public void deleteTest() {
-        MenuVo rtnMenuVo = this.menuService.delete(1);
+        Menu rtnMenuVo = this.menuService.delete(1);
 
         assertThat(rtnMenuVo.getName()).isEqualTo("file 1");
         assertThat(rtnMenuVo.isOpened()).isEqualTo(false);
@@ -64,19 +64,19 @@ public class MenuCrudTest {
 
     @Test
     public void findByIdTest() {
-        MenuVo rtnMenuVo = this.menuService.findById(2);
+        Menu rtnMenuVo = this.menuService.findById(2);
 
         assertThat(rtnMenuVo.getName()).isEqualTo("file 2");
         assertThat(rtnMenuVo.isOpened()).isEqualTo(true);
     }
     @Test
     public void updateTest() {
-        MenuVo menuVo = new MenuVo();
+        Menu menuVo = new Menu();
         menuVo.setId(1);
         menuVo.setName("file 4");
         menuVo.setOpened(true);
 
-        MenuVo rtnMenuVo = this.menuService.update(menuVo);
+        Menu rtnMenuVo = this.menuService.update(menuVo);
 
         assertThat(rtnMenuVo.getName()).isEqualTo("file 4");
         assertThat(rtnMenuVo.isOpened()).isEqualTo(true);
@@ -84,9 +84,9 @@ public class MenuCrudTest {
 
     @Test
     public void findAllQueryDslTest() {
-        QMenuVo qMenuVo = QMenuVo.menuVo;
-        Predicate predicate = qMenuVo.isOpened.eq(false);
-        Iterable<MenuVo> menuVos =  this.menuService.findAll(predicate);
+        QMenu qMenu = QMenu.menu;
+        Predicate predicate = qMenu.isOpened.eq(false);
+        Iterable<Menu> menuVos =  this.menuService.findAll(predicate);
         assertThat(menuVos).isNotNull().hasSize(3);
     }
 }
