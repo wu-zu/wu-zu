556d8b8a3dab07cbe7b6728c0c010febbb8468a0
krevis <venzersiz@gmail.com>
2016-02-21 00:11:40 +0900

    #37 현재 사용하지 않는 것을 미리 만들어두지 말자.

--- wu-zu-home/src/main/java/com/wuzu/home/interceptor/CommonInterceptor.java
+++ wu-zu-home/src/main/java/com/wuzu/home/interceptor/CommonInterceptor.java
@@ -6,8 +6,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.wuzu.home.domain.Common;
-
 /**
  * @author Gahngha Ryu
  */
@@ -16,34 +14,11 @@ public class CommonInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         
-        Common common = new Common();
-        
-        request.setAttribute("common", common);
-        
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        
-        String requestUri = request.getRequestURI();
-        
-        String target = requestUri.substring(requestUri.lastIndexOf("/") + 1);
-
-        int dotIndex = target.lastIndexOf(".");
-        
-        String viewFileName = null;
-        
-        if (dotIndex == -1) {
-            viewFileName = target;
-        }
-        else {
-            viewFileName = target.substring(0, dotIndex);
-        }
-        
-        Common common = (Common) request.getAttribute("common");
-        
-        common.setViewFileName(viewFileName);
     }
 
     @Override
