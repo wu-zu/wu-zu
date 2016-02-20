205b5b84c1abd105c936835fef418ba1f9962f4b
krevis <venzersiz@gmail.com>
2016-02-20 23:31:20 +0900

    resolve #43 HyperSQL로 변경완료

---
+++ wu-zu-proto-mybatis/src/main/resources/com/wuzu/proto/sql/init-data.sql
@@ -0,0 +1,7 @@
+INSERT INTO MENU (NAME, IS_OPENED) VALUES
+  ('file 1', false ),
+  ('file 2', true ),
+  ('folder 1', false),
+  ('file 3', true ),
+  ('folder 2', false )
+;
--- wu-zu-proto-mybatis/src/test/java/com/wuzu/proto/MyBatisTest.java
+++ wu-zu-proto-mybatis/src/test/java/com/wuzu/proto/MyBatisTest.java
@@ -5,13 +5,17 @@ import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.wuzu.proto.dao.MenuMapper;
+import com.wuzu.proto.vo.Menu;
 
 /**
  * @author Gahngha Ryu
@@ -23,10 +27,20 @@ public class MyBatisTest {
     @Autowired
     private SqlSession sqlSession;
     
-    @Ignore // TODO 류강하 : 메모리 디비로 변경할 것!
+    @Autowired
+    private MenuMapper menuMapper;
+    
     @Test
     public void connection() {
 
         assertThat(sqlSession.getConnection(), is(not(nullValue())));
+    }
+    
+    @Test
+    public void countMenus() {
+        
+        List<Menu> menus = menuMapper.selectAllMenus();
+
+        assertThat(menus.size(), is(5));
     }
 }
